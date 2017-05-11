7e46f0db49ffee43d83ebf79e2754b029f290c22
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2017-05-11 16:25:46 +0200

    Add config file

---
+++ emacs_config/config.el
@@ -0,0 +1,23 @@
+
+
+;;; shell stuff
+(setenv "PATH" (concat (getenv "PATH") ":/Users/leonhardtobisch/.nvm/versions/node/v6.7.0/bin"))
+(setq explicit-shell-file-name "/bin/bash")
+
+;; (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+
+;; (add-hook 'eshell-preoutput-filter-functions
+;;           'ansi-color-filter-apply)
+
+
+;;; indentation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq tab-width 2)
+;;(setq js-indent-level 2)
+
+(put 'scroll-left 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))

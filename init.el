50a0c5307093abfebd3854f581a24c58e2136b0f
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2018-07-25 16:22:55 +0200

    Update

--- init.el
+++ init.el
@@ -65,27 +65,34 @@ tangled, and the tangled file is compiled."
                   (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
+(defun org-mode-startup ()
+  (org-content 3))
+
+(add-hook 'org-mode-hook 'org-mode-startup)
+
 (setq org-src-fontify-natively t)
-     (use-package auctex
-       :ensure t
-       :defer t
-       :config(progn
-                (setq ispell-program-name "/usr/local/bin/aspell")
-                (ispell-change-dictionary "de" t)
-                (flyspell-mode 1)
-                (setq TeX-PDF-mode t)
-                (setq TeX-auto-save t)
-                (setq TeX-parse-self t)))
+(use-package auctex
+  :ensure t
+  :defer t
+  :config
+  (defvar ispell-program-name)
+  (setq ispell-program-name "/usr/local/bin/aspell"
+        TeX-PDF-mode t
+        TeX-auto-save t
+        TeX-parse-self t)
+  (ispell-change-dictionary "de" t)
+  (flyspell-mode 1))
+
 (require 'ox-latex)
 (unless (boundp 'org-latex-classes)
   (setq org-latex-classes nil))
 (add-to-list 'org-latex-classes
              '("myarticle" "\\documentclass[11pt]{myarticle}"
-  ("\\section{%s}" . "\\section*{%s}")
-  ("\\subsection{%s}" . "\\subsection*{%s}")
-  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
 
@@ -358,131 +365,89 @@ tangled, and the tangled file is compiled."
 
   (add-hook 'python-mode-hook 'my/python-mode-hook))
 
+(defvar js-ts-prettify-symbols-alist
+  '(("<=" . ?≤)
+    ("&&" . ?∧)
+    ("||" . ?∨)
+    (">=" . ?≥)
+    ("=>" . ?⟹)
+    ("!==" . ?≠)))
+
 (use-package js2-mode
   :ensure t
   :defer 1
   :mode "\\.js$"
   :config
-    (add-hook 'js2-mode-hook 'prettify-symbols-mode)
-    (add-hook 'js2-mode-hook
-              (lambda ()
-                'prettify-symbols-mode
-                (push '("<=" . ?≤) prettify-symbols-alist)
-                (push '(">=" . ?≥) prettify-symbols-alist)
-                (push '("=>" . ?⟹) prettify-symbols-alist)
-                (push '("!==" . ?≠) prettify-symbols-alist)))
-    (font-lock-add-keywords 'js2-mode
-                            '(("require" . font-lock-keyword-face)))
-    (setq
-     js-indent-level 2
-     js2-basic-offset 2
-     js2-bounce-indent-p t
-     js2-strict-missing-semi-warning nil
-     js2-concat-multiline-strings nil
-     js2-include-node-externs t
-     js2-skip-preprocessor-directives t
-     js2-strict-inconsistent-return-warning nil))
+  (add-hook 'js2-mode-hook 'prettify-symbols-mode)
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (setq-local prettify-symbols-alist js-ts-prettify-symbols-alist)
+              ))
+  (font-lock-add-keywords 'js2-mode
+                          '(("require" . font-lock-keyword-face)))
+  (setq
+   js-indent-level 2
+   js2-basic-offset 2
+   js2-bounce-indent-p t
+   js2-strict-missing-semi-warning nil
+   js2-concat-multiline-strings nil
+   js2-include-node-externs t
+   js2-skip-preprocessor-directives t
+   js2-strict-inconsistent-return-warning nil))
 
 (use-package indium
   :ensure t)
 
-(defun is-current-line-end-of-function ()
-    "returns nil if current line is end of a function expression"
-    (string-match-p ")\\(: [^ ]*\\)?\\ \\(=> \\)?{" (thing-at-point 'line t)))
+(defun setup-tide-mode()
+  (interactive)
+  (tide-setup)
+    ;; (flycheck-mode +1)
+    ;; (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    (company-mode +1))
 
-     (use-package tide
-       :ensure t
-       :defer 1
-       :bind (("C-c <up>" . tide-jump-to-definition))
-       :config
-       (progn
-         (add-hook 'typescript-mode-hook #'setup-tide-mode)
-         (add-hook 'js2-mode-hook #'setup-tide-mode)
-         (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
-         (setq tide-format-options '(
-                                 :insertSpaceAfterFunctionKeywordForAnonymousFunctions t
-                                 :placeOpenBraceOnNewLineForFunctions nil))))
+(use-package tide
+  :ensure t
+  :defer 1
+  :bind (("C-c <up>" . tide-jump-to-definition))
+  :config
+    (add-hook 'typescript-mode-hook #'setup-tide-mode)
+    (add-hook 'js2-mode-hook #'setup-tide-mode)
+    (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
+    (setq tide-format-options '(
+                            :insertSpaceAfterFunctionKeywordForAnonymousFunctions t
+                            :placeOpenBraceOnNewLineForFunctions nil)))
 
-     (use-package typescript-mode
-       :ensure t
-       :mode "\\.ts$"
-       :config
-       (setq typescript-indent-level 2)
-       (add-hook 'typescript-mode-hook 'prettify-symbols-mode)
-       (add-hook 'typescript-mode-hook
-                 (lambda ()
-                   'prettify-symbols-mode
-                   (push '("<=" . ?≤) prettify-symbols-alist)
-                   (push '(">=" . ?≥) prettify-symbols-alist)
-                   (push '("=>" . ?⟹) prettify-symbols-alist)
-                   (push '("!==" . ?≠) prettify-symbols-alist)))
-       (defun typescript--proper-indentation (parse-status)
-         "Overwriting original function in order to fix multiparam/newline indentation"
-         (save-excursion
-           (back-to-indentation)
-           (cond ((nth 4 parse-status)
-                  (typescript--get-c-offset 'c (nth 8 parse-status)))
-                 ((nth 8 parse-status) 0) ; inside string
-                 ((typescript--ctrl-statement-indentation))
-                 ((eq (char-after) ?#) 0)
-                 ((save-excursion (typescript--beginning-of-macro)) 4)
-                 ((nth 1 parse-status)
-                  (let ((same-indent-p (looking-at
-                                        "[]})]\\|\\_<case\\_>\\|\\_<default\\_>"))
-                        (continued-expr-p (typescript--continued-expression-p)))
-                    (goto-char (nth 1 parse-status))
-                    (if (looking-at "[({[]\\s-*\\(/[/*]\\|$\\)")
-                        (progn
-                          (skip-syntax-backward " ")
-                          (when (eq (char-before) ?\)) (backward-list))
+(use-package typescript-mode
+  :ensure t
+  :mode "\\.ts$"
+  :config
+  (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode-hook 'prettify-symbols-mode)
+  (add-hook 'typescript-mode-hook
+            (lambda ()
+              (setq-local prettify-symbols-alist js-ts-prettify-symbols-alist)
+              )))
 
 
-                          (if (is-current-line-end-of-function)
-                              (unless (string-match-p "[(]" (thing-at-point 'line t))
-                              (condition-case nil
-                                  (re-search-backward "[(]")
-                                (error nil))))
+(defun next-import ()
+  (condition-case nil
+      (progn
+        (re-search-forward "^import.*from.*$")
+        (move-beginning-of-line 1))
+    (error
+     (goto-char (point-max)))))
 
-                          (back-to-indentation)
-                          (cond (same-indent-p
-                                 (current-column))
-                                (continued-expr-p
-                                 (+ (current-column) (* 2 typescript-indent-level)
-                                    typescript-expr-indent-offset))
-                                (t
-                                 (+ (current-column) typescript-indent-level))))
-                      (unless same-indent-p
-                        (forward-char)
-                        (skip-chars-forward " \t"))
-                      (current-column))))
-                 ((typescript--continued-expression-p)
-                  (+ typescript-indent-level typescript-expr-indent-offset))
-                 (t 0)))))
-     (defun setup-tide-mode()
-       (interactive)
-       (tide-setup)
-         ;; (flycheck-mode +1)
-         ;; (setq flycheck-check-syntax-automatically '(save mode-enabled))
-         (eldoc-mode +1)
-         (tide-hl-identifier-mode +1)
-         (company-mode +1))
-     (defun next-import ()
-       (condition-case nil
-           (progn
-             (re-search-forward "^import.*from.*$")
-             (move-beginning-of-line 1))
-         (error
-          (goto-char (point-max)))))
+(defun import-start-key ()
+  (search-forward "'" nil nil)
+  ;; find  a better way to return nil
+  (quote nil))
 
-     (defun import-start-key ()
-       (search-forward "'" nil nil)
-       ;; find  a better way to return nil
-       (quote nil))
-
-     (defun import-sort ()
-         "Typescript/ES6 import sort"
-         (interactive)
-         (save-excursion
-           (goto-char (point-min))
-           (next-import)
-               (sort-subr nil 'next-import 'end-of-line 'import-start-key 'import-start-key)))
+(defun import-sort ()
+    "Typescript/ES6 import sort"
+    (interactive)
+    (save-excursion
+      (goto-char (point-min))
+      (next-import)
+          (sort-subr nil 'next-import 'end-of-line 'import-start-key 'import-start-key)))
